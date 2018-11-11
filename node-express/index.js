a946b823cd13cacf9470076fcc9cf2da59471bdd
Andreas Gropp <starmaerker@googlemail.com>
2018-11-11 09:54:47 +0100

    added body-parser

--- node-express/index.js
+++ node-express/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/dishes', (req, res, next) => {
+    res.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes', (req, res, next) => {
+    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+});
 
 app.use(express.static(__dirname+ '/public'));
 
